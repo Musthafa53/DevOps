57ce6d24c5970b5dd39b40985930262e3dffc711
Musthafa53 <88128054+Musthafa53@users.noreply.github.com>
2023-10-31 20:43:16 +0530

    Create Maven Install.sh

---
+++ Maven Install.sh
@@ -0,0 +1,4 @@
+wget https://dlcdn.apache.org/maven/maven-3/3.9.5/binaries/apache-maven-3.9.5-bin.tar.gz
+tar -zxvf apache-maven-3.9.5-bin.tar.gz
+yum install java-1.8.0-openjdk -y
+yum install maven -y
