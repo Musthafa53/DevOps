6f2093e25ee7f2b390d606b7fbe4b42e53c74ea1
Musthafa53 <88128054+Musthafa53@users.noreply.github.com>
2023-11-07 14:46:31 +0530

    Update Jenkins Install.sh

--- Jenkins Install.sh
+++ Jenkins Install.sh
@@ -3,4 +3,3 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install jenkins -y
 systemctl start jenkins
-yum install git -y
